98bc417bea9294a7828e2a7d4b925b7ed4ab8017
krsxianley <christianneleyubana30@gmail.com>
2026-05-23 00:26:21 +0800

    FIXED DB

--- config/database.php
+++ config/database.php
@@ -23,6 +23,7 @@ function db(): PDO
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_TIMEOUT => 5,
     ];
 
     $pdo = new PDO($dsn, $user, $pass, $options);
